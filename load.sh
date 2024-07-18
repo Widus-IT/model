104120a7e055de90944aa3fbcefe53b5d08e4328
bhjun-widus <bhjun@widus-it.com>
2024-07-18 12:54:03 +0900

    parameters 추가

--- load.sh
+++ load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NAME=$1
-curl http://localhost:8080/models/apply -H "Content-Type: application/json" -d '{
+curl http://localhost:8080/models/apply -H "Content-Type: application/json" -d "{
      "url": "https://raw.githubusercontent.com/Widus-IT/model/main/${NAME}.yaml",
      "name": "${NAME}"
-   }'  
+   }"  
